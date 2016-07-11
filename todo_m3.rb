fafd8f7bdbdd263ad689213506942db6229cbc95
anta-tinypulse <anta@tinypulse.com>
2016-07-11 11:15:56 +0700

    milestone 3 again

---
+++ todo_m3.rb
@@ -0,0 +1,47 @@
+class Todo
+  def initialize
+    @title = "Todo"
+    @items  = ["Decide supervisor and driver roles", "Implement Milestone 1"]
+  end
+end
+
+class HtmlTodo < Todo
+  def display
+    str = "<html>
+      <head>
+        <title>#{@title}</title>
+      </head>
+      <body>
+        <ul>\n"
+
+    @items.each do |item|
+      str << "          <li>#{item}</li>\n"
+    end
+
+    str << "        </ul>
+      <body>
+    </html>"
+    puts str
+  end
+end
+
+class PlainTodo < Todo
+  def display
+    str = "*** Todo ***\n"
+
+    @items.each do |item|
+      str << "- #{item}\n"
+    end
+
+    puts str
+  end
+end
+
+# Pros:
+# - no need for if else
+# - seperate the logic into subclasses
+# - when we need to add one more case, just need to add a class, no need to change existing classes
+# Cons:
+# - lengthier
+# 
+#
