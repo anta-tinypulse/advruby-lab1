a0e910ad96a5bf1ee9a85bd483a8a1e01efd3f1d
anta-tinypulse <anta@tinypulse.com>
2016-07-11 10:56:01 +0700

    milestone 1

--- todo.rb
+++ todo.rb
@@ -3,4 +3,22 @@ class Todo
     @title = "Todo"
     @items  = ["Decide supervisor and driver roles", "Implement Milestone 1"]
   end
+
+  def display
+    str = "<html>
+      <head>
+        <title>#{@title}</title>
+      </head>
+      <body>
+        <ul>\n"
+
+    @items.each do |item|
+        str << "          <li>#{item}</li>\n"
+    end
+
+    str << "        </ul>
+      <body>
+    </html>"
+    puts str
+  end
 end
